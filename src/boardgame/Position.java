dc7dd299108929f621f6bc2bc987da5c12ae4cca
michelXD <michelXD@gmail.com>
2021-10-05 19:37:38 -0300

    movimentacao possiveis

--- src/boardgame/Position.java
+++ src/boardgame/Position.java
@@ -25,12 +25,13 @@ public class Position {
 	public void setColumn(int column) {
 		this.column = column;
 	}
-	
+	public void setValues(int row, int column) {
+		this.row = row;
+		this.column = column;
+	}
 	@Override
 	public String toString() {
-		return row 
-			  +", "
-			  +column;
+		return row +", " +column;
 	}
-
+	
 }
