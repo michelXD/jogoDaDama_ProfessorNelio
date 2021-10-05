98559dcdbb56df991ce20bbe74f783d3bdd0fdad
michelXD <michelXD@gmail.com>
2021-10-05 11:51:48 -0300

    att color

--- src/application/UI.java
+++ src/application/UI.java
@@ -1,8 +1,29 @@
 package application;
 
 import chess.ChessPiece;
+import chess.Color;
 
 public class UI {
+	
+	public static final String ANSI_RESET = "\u001B[0m";
+	public static final String ANSI_BACK = "\u001B[30m";
+	public static final String ANSI_RED = "\u001B[31m";
+	public static final String ANSI_GREEN = "\u001B[32m";
+	public static final String ANSI_YELLOW = "\u001B[33m";
+	public static final String ANSI_BLUE = "\u001B[34m";
+	public static final String ANSI_PURPLE = "\u001B[35m";
+	public static final String ANSI_CYAN = "\u001B[36m";
+	public static final String ANSI_WHITE = "\u001B[37m";
+	
+	public static final String ANSI_BACK_BLACKGROUND = "\u001B[40m";
+	public static final String ANSI_RED_BLACKGROUND = "\u001B[41m";
+	public static final String ANSI_GREEN_BLACKGROUND = "\u001B[42m";
+	public static final String ANSI_YELLOW_BLACKGROUND = "\u001B[43m";
+	public static final String ANSI_BLUE_BLACKGROUND = "\u001B[44m";
+	public static final String ANSI_PURPLE_BLACKGROUND = "\u001B[45m";
+	public static final String ANSI_CYAN_BLACKGROUND = "\u001B[46m";
+	public static final String ANSI_WHITE_BLACKGROUND = "\u001B[47m";
+	
 	
 	public static void printBoard(ChessPiece[][] pieces) {
 		for(int i=0; i<pieces.length; i++) {
@@ -19,8 +40,11 @@ public class UI {
 		if(piece == null) {
 			System.out.print("-");
 		}
+		else if(piece.getColor() == Color.WHITE){
+			System.out.print(ANSI_WHITE + piece + ANSI_RESET);
+		}
 		else {
-			System.out.print(piece);
+			System.out.print(ANSI_YELLOW + piece + ANSI_RESET);
 		}
 		System.out.print(" ");
 	}
